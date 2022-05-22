4443d14bf155b085902ab9e8610dad39212e2369
yakhyaD <yakhyadiom@gmail.com>
2022-05-22 00:41:51 +0000

    bootsrap project

---
+++ src/main/java/sn/yakhya_diome/book_rentals/BookRentalsApplication.java
@@ -0,0 +1,13 @@
+package sn.yakhya_diome.book_rentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRentalsApplication.class, args);
+	}
+
+}
