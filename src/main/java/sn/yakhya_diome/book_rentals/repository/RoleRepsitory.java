2c608d0f73906bd7e53b32eaa21165f2ce2df31a
yakhyaD <yakhyadiom@gmail.com>
2022-05-27 18:46:52 +0000

    implement repositories

---
+++ src/main/java/sn/yakhya_diome/book_rentals/repository/RoleRepsitory.java
@@ -0,0 +1,14 @@
+package sn.yakhya_diome.book_rentals.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import sn.yakhya_diome.book_rentals.models.ERole;
+import sn.yakhya_diome.book_rentals.models.Role;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepsitory extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(ERole name);
+}
---
+++ src/main/java/sn/yakhya_diome/book_rentals/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package sn.yakhya_diome.book_rentals.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import sn.yakhya_diome.book_rentals.models.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+}
